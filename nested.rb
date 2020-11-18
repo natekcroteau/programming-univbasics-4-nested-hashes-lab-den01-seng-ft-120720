813e6baba26dad9f3148f21e2a70c29d40b270c2
Nate Croteau <natekcroteau@gmail.com>
2020-11-18 23:47:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages] = languages
+programmer_hash[:dennis_ritchie][:languages] = language
 puts language
 end
 
