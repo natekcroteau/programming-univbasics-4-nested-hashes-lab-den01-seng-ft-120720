ae951e5b37ad231131fdde8c0e28df7a6d1c7498
Nate Croteau <natekcroteau@gmail.com>
2020-11-18 23:58:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"             # 1 points to a new index = "Assembly"
 programmer_hash
 end
