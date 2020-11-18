b2e683850010f5569d3c566fcae754b06eba4566
Nate Croteau <natekcroteau@gmail.com>
2020-11-18 23:58:42 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]                              #
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]                      #
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:dennis_ritchie][:languages][0]  #the 0 calls upon that index in languages array to return the string instead of array
 end
 
 def adding_matz
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {               #
+  :known_for => "Ruby",                                #
+  :languages => ["LISP", "C"]                          #
+}                                                      #
+programmer_hash                                        #
 end
 
 def changing_alan
@@ -103,7 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"  #
+programmer_hash                                 #
 end
 
 def adding_to_dennis
@@ -125,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"             # 1 points to a new index w/i languages to = "Assembly"
+programmer_hash
 end
